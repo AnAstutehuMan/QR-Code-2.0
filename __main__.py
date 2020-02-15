bcc8b32518d3aa9fa22d753c126120e2ff2e0061
Saketh Pabolu <51687055+AnAstutehuMan@users.noreply.github.com>
2020-02-15 15:30:51 -0700

    .

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ import os
 import shutil
 import time
 
+import PIL
 import qrcode
 from PIL import Image
 
@@ -11,18 +12,20 @@ from PIL import Image
 filename = "test.jpg"
 readablefile = open(filename, "rb")
 encoded = base64.b64encode(readablefile.read())
-
 # print(encoded)
-input()
 
 # Convert Base64 to QR Code
-qr = qrcode.QRCode(
-    version=40,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data(encoded)  # encoded is the data from Base64
-qr.make(fit=True)
+#qr = qrcode.QRCode(
+#    version=10,
+#    error_correction=qrcode.constants.ERROR_CORRECT_L,
+#    box_size=10,
+#    border=4,
+#)
+
+#qr.add_data('Hello there!')  # encoded is the data from Base64
+#qr.make(fit=True)
+
+img = qrcode.make(encoded)
 
 img = qr.make_image(fill_color="black", back_color="white")
+img.save("new_img.jpg")
