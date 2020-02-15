8d8074d76528c717a323ef4b6cd23fa394c3e7ed
Saketh Pabolu <51687055+AnAstutehuMan@users.noreply.github.com>
2020-02-15 15:05:49 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -4,13 +4,6 @@ import shutil
 import base64
 from PIL import Image     
 
-filename = "test.jpg"
-shutil.copyfile(filename,'copy-'+filename)
-covfile = 'copy-'+filename
-prefilename, preext = os.path.splitext(covfile)
-postfile = os.rename(covfile,prefilename+'.txt')
-postfile = open(postfile)
-encoded = base64.b64encode(postfile.read())
 
-print(encoded)
-input()
+
+
