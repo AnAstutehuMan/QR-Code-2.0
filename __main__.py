9a99384235a79208cb7f876a799a0f26ea21bba6
lynnlo <tharlynnhtet.ong@gmail.com>
2020-02-15 13:47:47 -0800

    added code

--- __main__.py
+++ __main__.py
@@ -1,2 +1,18 @@
+import time
+import math
+import pytesseract
+import os
+import shutil
 import base64
-import pytesseract 
+from PIL import Image     
+
+filename = "test.jpg"
+shutil.copyfile(filename,'copy-'+filename)
+covfile = 'copy-'+filename
+prefilename, preext = os.path.splitext(covfile)
+postfile = os.rename(covfile,prefilename+'.txt')
+postfile = open(postfile)
+encoded = base64.b64encode(postfile.read())
+
+print(encoded)
+input()
