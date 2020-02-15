162a941c25f0c3b41cb4ac2ae729443961ce5b16
lynnlo <tharlynnhtet.ong@gmail.com>
2020-02-15 14:11:06 -0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -6,9 +6,16 @@ import shutil
 import base64
 from PIL import Image     
 
+qr = qrcode.QRCode(
+    version=1,
+    box_size=20,
+    border=4
+)
+
 filename = "test.jpg"
 readablefile = open(filename, "rb")
 encoded = base64.b64encode(readablefile.read())
 
-print(encoded)
+qrcode.make(encoded)
+
 input()
