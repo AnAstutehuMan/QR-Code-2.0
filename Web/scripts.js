570776118c3c4d4abfbdea1063901af4a3d00586
Parker Storm <storm@localhost.localdomain>
2020-02-15 21:44:08 +0000

    ggggggggggggggggggg

--- Web/scripts.js
+++ Web/scripts.js
@@ -26,3 +26,32 @@ navigator.mediaDevices.getUserMedia(
     video.play();
     requestAnimationFrame(tick);
 }); 
+
+function tick() {
+    loadingMessage.innerText = " Loading Video... ";
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        loadingMessage.hidden = true;
+        canvasElement.hidden = false;
+        outputContainer.hidden = false;
+
+        canvasElement.height = video.videoHeight;
+        canvasElement.width = video.videoWidth;
+        canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+        var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+        var code = jsQR(imageData.data, imageData.width, imageData.height, {
+            inversionAttempts: "dontInvert"
+        });
+        if (code) {
+            drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
+            drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B358");
+            drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
+            outputMessage.hidden = true;
+            outputData.parentElement.hidden = false;
+            outputData.innerText = code.data;
+        } else {
+            outputMessage.hidden = false;
+            outputData.parentElement.hidden = true; 
+        }
+    }
+    requestAnimationFrame(tick);
+}
