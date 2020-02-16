8a3f6df0e9566b17c9487416de4e04edfe35e6fb
Saketh Pabolu <51687055+AnAstutehuMan@users.noreply.github.com>
2020-02-15 18:33:03 -0700

    new files

--- __main__.py
+++ __main__.py
@@ -9,28 +9,32 @@ import qrcode
 import qrcode.image.svg
 from PIL import Image
 
-filename = "test.jpg"
+filename = "test.png"
 
+# Resize Image
 newimg = Image.open(filename)
-newimg = newimg.resize((100, 100))
-newimg.convert('RGB').save('resized/'+filename)
+newimg = newimg.resize((50, 50))
+newimg.convert('RGB').save('resized/' + filename)
 
-readablefile = open('resized/'+filename, "rb")
+# Convert Image to Base64
+readablefile = open('resized/'+ filename, "rb")
 convfile = base64.b64encode(readablefile.read())
 
 # Convert Base64 to QR Code
 qr = qrcode.QRCode(
-    version=10,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=5,
+    version = 10,
+    error_correction = qrcode.constants.ERROR_CORRECT_L,
+    box_size = 10,
+    border = 5,
 )
+
 qr.add_data(convfile)  # encoded is the data from Base64
-print("starting")
-print('creating image | ver : '+str(qr.version))
+print("Starting")
+print('Creating image | ver: ' + str(qr.version))
 qr.make(fit=True)
 
+# Save the QR Code to Path
 img = qr.make_image(fill_color="black", back_color="white")
-img.save('QR/' + filename + 'QR')
+img.save('QR/' + filename + ' - QR')
 
 print("Done")
