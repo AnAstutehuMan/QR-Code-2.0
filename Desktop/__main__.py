7dbcd509e7d9d06b7eb1b6fc04f209b53a2f85c3
Saketh Pabolu <51687055+AnAstutehuMan@users.noreply.github.com>
2020-02-16 02:15:19 -0700

    GUI fix

--- Desktop/__main__.py
+++ Desktop/__main__.py
@@ -3,7 +3,7 @@ import math
 import os
 import shutil
 import time
-import tkinter
+from tkinter import *
 
 import PIL
 import qrcode
@@ -11,8 +11,8 @@ import qrcode.image.svg
 from PIL import Image
 
 
-def reSize(fileName):
-    newImg = Image.open(fileName)
+def reSize(path):
+    newImg = Image.open(path)
     width = 150
     ratio = width/float(newImg.size[0])
     height = int(float(newImg.size[1])*ratio)
@@ -20,32 +20,60 @@ def reSize(fileName):
     print(ratio)
 
     newImg = newImg.resize((width, height))
-    newImg.convert('RGB').save('Resized/'+fileName)
+    newImg.convert('RGB').save('Resized/'+path)
 
     # Convert Image to Base64
-    readablefile = open('Resized/' + fileName, "rb")
+    readablefile = open('Resized/' + path, "rb")
     convFile = base64.b64encode(readablefile.read())
 
-    base64ToQR(convFile, fileName)  
+    base64ToQR(convFile, path)
 
 
-
-def base64ToQR(convFile, fileName):
+def base64ToQR(convFile, path):
     qr = qrcode.QRCode(
-        version = 40,
-        error_correction = qrcode.constants.ERROR_CORRECT_L,
-        box_size = 10,
-        border = 5,
+        version=40,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=5,
     )
 
-    qr.add_data(convFile)  # Encoded is the data from Base64 (convFile is `str`)
+    # Encoded is the data from Base64 (convFile is `str`)
+    qr.add_data(convFile)
 
     print('Creating Image Ver: ' + str(qr.version))
 
     qr.make(fit=True)
     newImageToGenerate = qr.make_image(fill_color="black", back_color="white")
 
-    newImageToGenerate.save('QR/QR - ' + fileName)
+    newImageToGenerate.save('QR/QR - ' + path)
 
 
-reSize('test.jpg')
+window = Tk()
+window.title("QR Image")
+window.geometry('250x250')
+lbl = Label(window, text="Enter the Image path:  ")
+lbl.grid(column=0, row=0)
+
+fileName = Entry(window, width=10)
+fileName.grid(column=1, row=0)
+
+path = str(fileName)
+print(path)
+
+
+def clicked(path):
+    lbl.configure(text="Converting the Image to QR")
+    reSize(path)
+    print("Entered the Path!")
+
+
+path = str(fileName)
+print(path)
+
+btn1 = Button(window, text="Submit", command=clicked)
+btn1.grid(column=2, row=0)
+
+btn2 = Button(window, text="Quit", command=window.destroy)
+btn2.grid(column=3, row=0)
+
+window.mainloop()
