c334d79865e92014f62ef7a12f6899584689ebdb
Saketh Pabolu <51687055+AnAstutehuMan@users.noreply.github.com>
2020-02-15 18:35:18 -0700

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -17,15 +17,15 @@ newimg = newimg.resize((50, 50))
 newimg.convert('RGB').save('resized/' + filename)
 
 # Convert Image to Base64
-readablefile = open('resized/'+ filename, "rb")
+readablefile = open('resized/' + filename, "rb")
 convfile = base64.b64encode(readablefile.read())
 
 # Convert Base64 to QR Code
 qr = qrcode.QRCode(
-    version = 10,
-    error_correction = qrcode.constants.ERROR_CORRECT_L,
-    box_size = 10,
-    border = 5,
+    version=10,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=5,
 )
 
 qr.add_data(convfile)  # encoded is the data from Base64
@@ -35,6 +35,6 @@ qr.make(fit=True)
 
 # Save the QR Code to Path
 img = qr.make_image(fill_color="black", back_color="white")
-img.save('QR/' + filename + ' - QR')
+img.save('QR/' + filename)
 
 print("Done")
