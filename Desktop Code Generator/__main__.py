9129f3b5f4368cc1b694514955ebc7beeb42bef3
Saketh <51687055+spabolu@users.noreply.github.com>
2021-07-02 02:49:56 -0700

    renamed and moved files

--- Desktop Code Generator/__main__.py
+++ Desktop Code Generator/__main__.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import base64
 import math
 import os
@@ -18,38 +20,38 @@ fs = False
 def reSize(path):
     newImg = Image.open(os.path.join(rootdir, path))
     width = 100
-    ratio = width/float(newImg.size[0])
-    height = int(float(newImg.size[1])*ratio)
+    ratio = width / float(newImg.size[0])
+    height = int(float(newImg.size[1]) * ratio)
 
-    lbl.configure(text="Resizing the Image...")
+    lbl.configure(text='Resizing the Image...')
 
     newImg = newImg.resize((width, height))
     newImg.convert('RGB').save(os.path.join(rootdir, 'resized/' + path))
 
     # Convert Image to Base64
-    readablefile = open(os.path.join(rootdir, 'resized/' + path), "rb")
+
+    readablefile = open(os.path.join(rootdir, 'resized/' + path), 'rb')
     convFile = base64.b64encode(readablefile.read())
 
-    lbl.configure(text="Converting the Image to QR...")
+    lbl.configure(text='Converting the Image to QR...')
 
     base64ToQR(convFile, path)
 
 
 def base64ToQR(convFile, path):
-    qr = qrcode.QRCode(
-        version=5,
-        error_correction=qrcode.constants.ERROR_CORRECT_M,
-        box_size=4,
-        border=5,
-    )
+    qr = qrcode.QRCode(version=5,
+                       error_correction=qrcode.constants.ERROR_CORRECT_M,
+                       box_size=4, border=5)
 
     # Encoded is the data from Base64 (convFile is `str`)
+
     qr.add_data(convFile)
 
-    print('Creating Image Ver: ' + str(qr.version))
+    print 'Creating Image Ver: ' + str(qr.version)
 
     qr.make(fit=True)
-    newImageToGenerate = qr.make_image(fill_color="black", back_color="white")
+    newImageToGenerate = qr.make_image(fill_color='black',
+            back_color='white')
 
     newImageToGenerate.save(os.path.join(rootdir, 'qr/QR - ' + path))
 
@@ -62,17 +64,17 @@ def base64ToQR(convFile, path):
 def tooglefull():
     global fs
     if not fs:
-        window.attributes("-fullscreen", True)
+        window.attributes('-fullscreen', True)
         fs = True
     elif fs:
-        window.attributes("-fullscreen", False)
+        window.attributes('-fullscreen', False)
         fs = False
 
 
 window = tkinter.Tk()
-window.title("QR Image")
-window.geometry("500x250")
-lbl = tkinter.Label(window, text="Enter the Image path:  ")
+window.title('QR Image')
+window.geometry('500x250')
+lbl = tkinter.Label(window, text='Enter the Image path:  ')
 lbl.config(width=25)
 lbl.grid(column=0, row=0)
 
@@ -83,18 +85,18 @@ fileName.grid(column=1, row=0)
 
 def clicked():
     path = fileName.get()
-    print(path)
-    lbl.configure(text="Processing the Image...")
+    print path
+    lbl.configure(text='Processing the Image...')
     reSize(path)
-    lbl.configure(text="Enter the Image path :  ")
-    print("Entered the Path!")
+    lbl.configure(text='Enter the Image path :  ')
+    print 'Entered the Path!'
 
 
-btn1 = tkinter.Button(window, text="Submit", command=clicked)
+btn1 = tkinter.Button(window, text='Submit', command=clicked)
 btn1.config(width=10)
 btn1.grid(column=2, row=0)
 
-btn1 = tkinter.Button(window, text="FullScreen", command=tooglefull)
+btn1 = tkinter.Button(window, text='FullScreen', command=tooglefull)
 btn1.config(width=10)
 btn1.grid(column=3, row=0)
 
